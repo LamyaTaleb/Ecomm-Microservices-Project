bb8270d4705bc1a30bdfa8028ec90f692bc3dd35
Lamya Taleb <lamyataleb2001@gmail.com>
2022-11-30 11:54:45 +0100

    circuit breaker step

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.mproduits.web.controller;
 
+import com.mproduits.configurations.ApplicationPropertiesConfiguration;
 import com.mproduits.dao.ProductDao;
 import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,17 +20,23 @@ public class ProductController {
     @Autowired
     ProductDao productDao;
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    ApplicationPropertiesConfiguration appProperties;
+
     // Affiche la liste de tous les produits disponibles
     @GetMapping(value = "/Produits")
     public List<Product> listeDesProduits(){
-
         List<Product> products = productDao.findAll();
-
         if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
+                List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
+        log.info("Récupération de la liste des produits");
 
-        return products;
-
+        return listeLimitee;
     }
+
+
+
 
     //Récuperer un produit par son id
     @GetMapping( value = "/Produits/{id}")
